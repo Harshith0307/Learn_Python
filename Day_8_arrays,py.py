20fbe43da83fb78471324709863942c59713ce6c
Harshith0307 <51245050+Harshith0307@users.noreply.github.com>
2019-12-31 20:17:30 -0600

    Solution to phonebook; intro to dictionaries

---
+++ Day_8_arrays,py.py
@@ -0,0 +1,20 @@
+mydict = {}
+
+n = int(input())
+for item in range(n):
+    data = list(input().rstrip().split())
+    mydict[data[0]] = data[1]
+
+mydict_keys = mydict.keys()
+
+while True:
+    try:
+        name = input()
+        if name in mydict_keys:
+            print(name + "=" + mydict[name])
+        else:
+            print("Not found")
+    except:
+        break
+
+
